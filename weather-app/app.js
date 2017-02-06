dea50dbdab719477cee8ce5e58fb3db813e30fd4
mallek <travis@haleycomputersolutions.com>
2017-02-05 23:02:04 -0700

    working google api check

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,42 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const addressOptions = {
+    describe: 'Address to search weather for',
+    demand: true,
+    alias: 'a',
+    string: true
+};
+
+const argv = yargs
+    .option({
+        address: addressOptions
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv);
 
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=561%20e%20caley%20dr%20denver%20co',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
     json: true
 }, (error, response, body) => {
     //pretty print json
-    console.log(JSON.stringify(response, undefined, 2));
+    // console.log(JSON.stringify(body, undefined, 2));
+
+    if (error) {
+        console.log('unable to connect to servers.', error);
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('unable to find that address.');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    } else {
+        console.log('Unknown Error ', body);
+    }
+
+
 });
 
