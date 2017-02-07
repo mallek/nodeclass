22e7180d6250ed9dca87ae60596e5a319622014e
mallek <travis@haleycomputersolutions.com>
2017-02-06 21:01:27 -0700

    refactores promises2 to use video solution

--- weather-app/playground/promise-2.js
+++ weather-app/playground/promise-2.js
@@ -1,19 +1,38 @@
-const geocode = require('../geocode/geocode');
+const request = require('request');
 
 var geocodeAddress = (address) => {
     return new Promise((resolve, reject) => {
-        geocode.geocodeAddress(address, (errorMessage, results) => {
-            if (errorMessage) {
-                reject(errorMessage);
+        request({
+            url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}`,
+            json: true
+        }, (error, response, body) => {
+            //pretty print json
+            // console.log(JSON.stringify(body, undefined, 2));
+
+            if (error) {
+                reject('unable to connect to servers.');
+            } else if (body.status === 'ZERO_RESULTS') {
+                reject('unable to find that address.');
+            } else if (body.status === 'OK') {
+                resolve({
+                    address: body.results[0].formatted_address,
+                    latitude: body.results[0].geometry.location.lat,
+                    longitude: body.results[0].geometry.location.lng,
+                });
+                // console.log(`Address: ${body.results[0].formatted_address}`);
+                // console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
+                // console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
             } else {
-                resolve(results);
+                reject('Unknown Error -- ' + body);
             }
+
+
         });
     })
 };
 
 
-geocodeAddress('00000').then((location) => {
+geocodeAddress('80120').then((location) => {
     console.log(JSON.stringify(location, undefined, 2));
 }, (errorMessage) => {
     console.log(errorMessage);
