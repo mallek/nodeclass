eb04a3c1db2e7f3f86be09fb200f9716ab6292ce
Travis Haley <thaley@dlr360.com>
2017-02-11 14:20:50 -0700

    Added sunrise api

--- weather-app/app-promise.js
+++ weather-app/app-promise.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const axios = require('axios');
+const moment = require('moment');
 
 const addressOptions = {
     describe: 'Address to search weather for',
@@ -18,6 +19,8 @@ const argv = yargs
 
 const apiKey = '0e68fad128467875ece1b17342e3d252'
 
+var lat;
+var lng;
 
 var encodedAddress = encodeURIComponent(argv.address);
 geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`
@@ -27,16 +30,24 @@ axios.get(geocodeUrl).then((response) => {
         throw new Error('Unable to find that address');
     }
 
-    var lat = response.data.results[0].geometry.location.lat;
-    var lng = response.data.results[0].geometry.location.lng;
+    lat = response.data.results[0].geometry.location.lat;
+    lng = response.data.results[0].geometry.location.lng;
     var weatherUrl = `https://api.darksky.net/forecast/${apiKey}/${lat},${lng}`;
     console.log(response.data.results[0].formatted_address);
     return axios.get(weatherUrl);
 }).then((response) => {
     var temp = response.data.currently.temperature;
     var apparentTemp = response.data.currently.apparentTemperature;
-
+    console.log(JSON.stringify(response.data, undefined, 2))
     console.log(`It's currently ${temp} it feels like ${apparentTemp}`)
+    var sunriseUrl = `http://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`
+    return axios.get(sunriseUrl);
+    }).then((response) => {
+      //  console.log(response.data);
+    var sunrise = response.data.results.sunrise;
+    var sunset = response.data.results.sunset;
+    console.log(`moment test ${moment.unix(1486476162).format('h:mm:ss a')}`)
+    console.log(`Sunrise: ${moment(sunrise).format('h:mm:ss a')}  Sunset: ${moment(sunset).format('h:mm:ss a')}`);
 }).catch((e) => {
     if (e.code === 'ENOTFOUND') {
         console.log('unable to connect to api server')
