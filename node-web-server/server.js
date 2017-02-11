f88b3975aaf118c473d9e54f350abd0c8b59b13a
mallek <travis@haleycomputersolutions.com>
2017-02-11 02:52:17 -0700

    added projects

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -55,6 +55,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+    });
+});
+
 app.get('/json', (req, res) => {
     res.send({
         name: 'Travis',
