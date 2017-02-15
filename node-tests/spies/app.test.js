ee21539b1dacedbfac7b4a84709f04b352765251
mallek <travis@haleycomputersolutions.com>
2017-02-14 19:41:26 -0700

    testing tests

--- node-tests/spies/app.test.js
+++ node-tests/spies/app.test.js
@@ -3,20 +3,22 @@ const rewire = require('rewire');
 
 var app = rewire('./app');
 
-describe('App', () => {
+
+describe('App', function () {
     var db = {
         saveUser: expect.createSpy()
     };
     app.__set__('db', db);
 
-    it('should call the spy correctly', () => {
+
+    it('should call the spy correctly', function () {
         var spy = expect.createSpy();
         spy('Travis', 25);
 
         expect(spy).toHaveBeenCalledWith('Travis', 25);
     });
 
-    it('should call saveUser with user object', () => {
+    it('should call saveUser with user object', function () {
         var email = 'travis@gmail.com';
         var password = 'qwerty';
 
